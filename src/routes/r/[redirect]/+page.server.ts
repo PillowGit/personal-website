f6183a5ab45b4815cf9892a9fa9f1650505c28a9
PillowGit <goldentaco727@gmail.com>
2024-04-19 21:52:12 -0700

    Fixed type error in server.ts

--- src/routes/r/[redirect]/+page.server.ts
+++ src/routes/r/[redirect]/+page.server.ts
@@ -1,4 +1,11 @@
-import redirects from '$lib/resources/redirects.json';
+// Get redirect data from json file
+interface Redirects {
+    [key: string]: string;
+}
+import data from '$lib/resources/redirects.json';
+const redirects: Redirects = data;
+
+// Import redirect function from sveltekit
 import { redirect } from '@sveltejs/kit';
 
 export function load({ params }) {
