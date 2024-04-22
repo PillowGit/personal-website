e99c8442aeea05ed08fb1ba6aaf07a2969a41c99
PillowGit <goldentaco727@gmail.com>
2024-04-22 15:06:26 -0700

    Added back redirect function

--- src/routes/r/[redirect]/+page.server.ts
+++ src/routes/r/[redirect]/+page.server.ts
@@ -10,8 +10,8 @@ import { redirect } from '@sveltejs/kit';
 
 export function load({ params }) {
     const url = redirects[params.redirect];
-    if (!url) {
+    if (url !== undefined) {
         throw redirect(302, '/');
     }
-    return { url };
+    return {};
 }
