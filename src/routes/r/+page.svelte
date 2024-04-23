9ce29f089a4805bb29038a3bbf80ba7b773ca0b4
PillowGit <goldentaco727@gmail.com>
2024-04-23 14:48:14 -0700

    Changed favicon to smth random

--- src/routes/r/+page.svelte
+++ src/routes/r/+page.svelte
@@ -8,34 +8,37 @@
   <p>If you're seeing this, you either typed a url wrong or came from the home page</p>
   <p>Here's a list of all the redirects available:</p>
   {#each links as [redirect, url]}
+  <div class="redirect">
     <a href={url} target="_blank">
-      <p class="redirect">pillo.dev/r/{redirect}</p>
+      pillo.dev/r/{redirect}
     </a>
+  </div>
   {/each}
 </div>
   
 <style>
   /* Style general page layout */
   .container {
-    /* Format text */
     text-align: left;
   
-    /* Center the container slightly */
     margin-left: 10%;
     margin-right: 10%;
     margin-top: 5%;
     margin-bottom: 5%;
     
-    /* Size the container */
     height: 90%;
     width: 80%;
   }
   .redirect {
-    /* Format text */
-    font-size: 3rem;
+    font-size: 2.5rem;
     margin-top: 5rem;
   }
-  /* Size text */
+  .redirect a:link, .redirect a:visited {
+    color: var(--main-color);
+  }
+  .redirect a:hover, .redirect a:active {
+    color: color-mix(in srgb, var(--main-color) 50%, transparent);
+  }
   h2, p {
     font-size: 2rem;
   }
