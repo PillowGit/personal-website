d683bc638636b9b003801f4127825ae87ed0500e
PillowGit <goldentaco727@gmail.com>
2024-04-24 00:21:12 -0700

    Working on shell (nowhere near done)

---
+++ src/lib/components/shell.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+    export let shell_title: null | string;
+
+    if (!shell_title) {
+        shell_title = "/home/pillow";
+    }
+</script>
+
+<div class="shell-container">
+    <div class="shell-navbar">
+        <div class="shell-icons"></div>
+        <div class="shell-title">
+            <h3>{shell_title}</h3>
+        </div>
+        <div class="shell-icons">
+            <img src="/icons/minimize.svg" alt="mini" />
+            <img src="/icons/maximize.svg" alt="max" />
+            <img src="/icons/x.svg" alt="x" />
+        </div>
+    </div>
+</div>
+
+<style>
+    :root {
+        --shell-outlines: color-mix(in srgb, var(--main-color) 70%, transparent);
+        --shell-lighter-bg: color-mix(in srgb, var(--shell-outlines) 13%, black);
+        --shell-darker-bg: color-mix(in srgb, var(--main-color) 12.5%, black);
+    }
+    .shell-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        margin-left: 5%;
+        min-width: 90%;
+        min-height: 20rem;
+
+        background-color: var(--shell-darker-bg);
+        border-radius: 1rem;
+        border: 0.2rem solid var(--shell-outlines);
+        font-family: "Martian Mono", monospace;
+    }
+    .shell-navbar {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        border-bottom: 0.2rem solid var(--shell-outlines);
+        border-top-left-radius: 1rem;
+        border-top-right-radius: 1rem;
+        width: 100%;
+        background-color: var(--shell-lighter-bg);
+    }
+    .shell-icons {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        justify-self: flex-end;
+
+        padding-right: 1rem;
+        width: 7.5rem;
+    }
+    .shell-icons>img {
+        height: 2rem;
+        aspect-ratio: 1/1;
+        -webkit-filter: invert(.7);
+        filter: invert(.7);
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import Navbar from "$lib/components/navbar.svelte";
+    import Shell from "$lib/components/shell.svelte";
 
     let navbar_choice: string = "";
     let navbar_index: number = -1;
@@ -7,12 +8,17 @@
 
 <Navbar bind:selection={navbar_choice} bind:index={navbar_index}/>
 <div class="page">
-    <h1>
+    <h1 class="page-title">
         Pillow
     </h1>
-    <p>
-        You picked:<br>{navbar_index+1}. {navbar_choice} 
-    </p>
+
+    <div class="about-me-container">
+        <h2 class="about-me-title">
+            About Me:
+        </h2>
+        <Shell shell_title="/home/pillow/about/"/>
+    </div>
+
 </div>
 
 <style>
@@ -24,11 +30,25 @@
         flex-direction: column;
         align-items: center;
     }
-    h1 {
+    .page-title {
         text-align: center;
-        font-size: 2.75rem;
+        font-size: 4rem;
 
-        height: 3.25rem;
-        min-width: 10rem;
+        min-height: 3.25rem;
+        min-width: 16rem;
+    }
+
+    .about-me-container {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        justify-content: center;
+
+        width: 60%;
+        height: 100%;
+    }
+    .about-me-title {
+        font-size: 2rem;
+        margin-left: 15%;
     }
 </style>
